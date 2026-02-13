eb50ca264cf64b31d93e79d2ad67e2ccfa6dc48d
Blaze Burgess <blaze.i.burgess@gmail.com>
2026-02-13 10:43:14 -0500

    Export Code Interpreter components from `index.ts`
    - Add exports for core Code Interpreter components:
      - `CodeExecutionIndicator`
      - `CodeInterpreterOutput`
      - `GeneratedFilePreview`
    - Centralize component exports to simplify imports and improve organization.

---
+++ components/Chat/ChatMessages/CodeInterpreter/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Code Interpreter Components
+ *
+ * Components for displaying Code Interpreter execution results in chat messages.
+ */
+
+export { CodeExecutionIndicator } from './CodeExecutionIndicator';
+export { CodeInterpreterOutput } from './CodeInterpreterOutput';
+export { GeneratedFilePreview } from './GeneratedFilePreview';
