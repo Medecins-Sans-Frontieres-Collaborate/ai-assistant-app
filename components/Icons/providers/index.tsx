b784fc52b0a10fc3a32ddd17abbfc25d4645b2d0
Blaze Burgess <blaze.i.burgess@gmail.com>
2026-07-06 10:10:32 -0400

    Add MistralIcon to provider icons
    - Introduce `MistralIcon` component in `components/Icons/providers/`.
    - Update `index.tsx` to export the new `MistralIcon` alongside existing icons.

--- components/Icons/providers/index.tsx
+++ components/Icons/providers/index.tsx
@@ -2,6 +2,7 @@ export { OpenAIIcon } from './OpenAIIcon';
 export { DeepSeekIcon } from './DeepSeekIcon';
 export { XAIIcon } from './XAIIcon';
 export { MetaIcon } from './MetaIcon';
+export { MistralIcon } from './MistralIcon';
 export { AzureAIIcon } from './AzureAIIcon';
 export { AzureOpenAIIcon } from './AzureOpenAIIcon';
 export { ClaudeAIIcon, ClaudeAIIconBrand } from './ClaudeAIIcon';
