3e734b1c6f13727102ece1b15f90c34c2c6b3b22
Blaze Burgess <blaze.i.burgess@gmail.com>
2026-07-18 06:45:12 -0400

    Add MemoriesSection to SettingDialog component
    - Enable rendering of the Memories section UI when the active section
      is set to `SettingsSection.MEMORIES`.
    - Extend settings navigation to support user interaction with memory
      features.

--- components/Settings/SettingDialog.tsx
+++ components/Settings/SettingDialog.tsx
@@ -24,6 +24,7 @@ import { ConnectorsSection } from './Sections/ConnectorsSection';
 import { DataManagementSection } from './Sections/DataManagementSection';
 import { GeneralSection } from './Sections/GeneralSection';
 import { HelpSupportSection } from './Sections/HelpSupportSection';
+import { MemoriesSection } from './Sections/MemoriesSection';
 import { MobileAppSection } from './Sections/MobileAppSection';
 import { UsageImpactSection } from './Sections/UsageImpactSection';
 import { SettingsSidebar } from './SettingsSidebar';
@@ -328,6 +329,10 @@ export function SettingDialog() {
                 )}
 
                 {activeSection === SettingsSection.BACKUP && <BackupSection />}
+
+                {activeSection === SettingsSection.MEMORIES && (
+                  <MemoriesSection />
+                )}
 
                 {activeSection === SettingsSection.DATA_MANAGEMENT && (
                   <DataManagementSection
